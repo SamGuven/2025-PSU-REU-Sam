cd0421330735b7530e96a32c1addd2b03e2a4696
SamGuven <guvens@southwestern.edu>
2025-07-17 11:35:04 -0500

    Figured out how to disable image caching

---
+++ test_cuda_available.py
@@ -0,0 +1,13 @@
+import torch
+
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+print(torch.__version__)
