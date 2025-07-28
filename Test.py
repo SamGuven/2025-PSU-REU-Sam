634ed6a026bfd0f760df3fe4ad393d00e518c883
SamGuven <guvens@southwestern.edu>
2025-07-28 17:48:14 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 import cv2
 
-p = "/u/samguv_guest/2025-PSU-REU-Sam/2025-PSU-REU-Sam/datasets/ECP/img/train/REPLACE_WITH_A_REAL_FILENAME.png"
+p = "/u/samguv_guest/2025-PSU-REU-Sam/2025-PSU-REU-Sam/datasets/ECP/img/train/amsterdam_00000.png"
 img = cv2.imread(p)
 print("Loaded?", img is not None)
 if img is not None:
